de71b7fad0284dc5d1176ead827a0e072fec0719
livotieno <livio5261@gmail.com>
2023-10-05 16:33:11 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", function (e) {
+    //console.log(e.target);
+    fetch ("http://localhost:3000/characters")
+     .then(response => response.json())
+     .then((data) =>{
+     //console.log(data);
+     displayCharacters (data)
+
+     })
+})
+
+function displayCharacters(animalData){
+    console.log(animalData);
+    const characterBar=document.querySelector("#character-bar")
+    animalData.forEach(element => {
+      const span =document.createElement("span")  
+      span.innerHTML= element.name
+      characterBar.appendChild(span)
+
+      span.addEventListener("click",(e)=>{
+        handleSpanclick(e,element)
+      })
+
+        
+    });
+}
+function handleSpanclick(e,element){
+    console.log(e.target);
+    const name = document.querySelector("#name")
+    const image = document.querySelector("#image")
+    const voteCount = document.querySelector("#vote-count")
+    name.innerHTML = element.name
+    image.src = element.image
+   voteCount.innerHTML = element.votes
+   
+   const votesForm = document.querySelector("#votes-form")
+   votesForm.addEventListener("submit",(e)=>{
+    handleSubmission(e, voteCount)
+    
+
+   })
+}
+
+function handleSubmission(e, voteCount){
+    e.preventDefault()
+  const inputValue = document.querySelector("#votes").value
+  voteCount.innerHTML = inputValue
+}
